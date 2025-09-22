f477fdb94b238c2cd4a6126c28d8e4b71364aa91
sfrost142 <158857720+sfrost142@users.noreply.github.com>
2025-09-22 18:49:35 -0400

    overhaul

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+window.onload = function() {
+    var stylesheet = document.getElementById('stylesheet');
+    if (localStorage.getItem('colorblindMode') === 'true') {
+        stylesheet.setAttribute('href', 'style2.css');
+    }
+};
+
 document.getElementById('changeStyleButton').addEventListener('click', function() {
     var stylesheet = document.getElementById('stylesheet');
     if (stylesheet.getAttribute('href') === 'style.css') {
         stylesheet.setAttribute('href', 'style2.css');
+        localStorage.setItem('colorblindMode', 'true');
     } else {
         stylesheet.setAttribute('href', 'style.css');
+        localStorage.setItem('colorblindMode', 'false');
     }
 });
